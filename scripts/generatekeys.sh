f8d7901d5f76612de9cab688680aa61a503e460f
Camilo José del Real Martell <40240381+CamiloDelReal@users.noreply.github.com>
2022-08-22 02:35:45 +0200

    ADD: Script for generating keys

---
+++ scripts/generatekeys.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo [!] Creating keys
+openssl req -newkey rsa:2048 -new -nodes -keyout privatekey.pem -out csr.pem
+openssl rsa -in privatekey.pem -pubout > publickey.pem
+echo [+] Keys created
